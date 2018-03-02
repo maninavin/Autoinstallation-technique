a00200ac50a15a57b2385a21dd1d5425042d3377
Manikandan Subramaniam <gn.manikandan@gmail.com>
2018-03-01 21:00:46 -0800

    Add files via upload

---
+++ insatll.bat
@@ -0,0 +1,9 @@
+ "C:\Users\M.Subramaniam\Desktop\Shell-Assignment\TeamViewer_Setup.exe" /S /norestart
+ "\\192.168.4.64\qa\WebAutomation\Tools\pycharm-community-2017.1.1.exe" /S /norestart
+ "\\192.168.4.64\qa\WebAutomation\Tools\Git-2.15.1.2-64-bit.exe" /S /norestart
+ "\\192.168.4.64\qa\WebAutomation\Tools\vs_community.exe" /S /norestart
+ msiexec /i "\\192.168.4.64\qa\WebAutomation\Tools\python-2.7.msi" /qn /norestart
+"C:\LDSWebTests\_testdata\Fingerprint\Win32 regsvr32 DPDeviceDumb.dll /qn /norestart
+
+
+
